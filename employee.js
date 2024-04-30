e7e57a1866811ee98f891191da70e998a9f95b46
Kendra <cruickshank.kendra@gmail.com>
2024-04-29 17:52:29 -0700

    fix sayName

--- employee.js
+++ employee.js
@@ -1,1 +1,17 @@
 // Your code here 
+
+class Employee {
+    constructor(name, occupation){
+        this.name = name;
+        this.occupation = occupation;
+    }
+    sayName(){
+        console.log(`${this.name} says hello`);
+    }
+
+}
+
+// const employee1 = new Employee("John Wick", "Dog Lover")
+// sayName(employee1)
+
+module.exports = Employee
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 // Your code here 
+const Employee = require('./employee.js')
+
+const employee1 = new Employee("John Wick", "Dog Lover")
+
+
+let boundedSayName = sayName.bind(employee1)
+setTimeout(boundedSayName, 2000)
+
+let boundSayOccupation = sayOccupation.bind(employee1)
+setTimeout(boundSayOccupation, 3000)
